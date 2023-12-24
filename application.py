12ed8307bfcb65d62e5bd5cd9bc487162c87b71b
항붕이 <mjdh1936@gmail.com>
2023-12-24 14:29:27 +0000

    feat, style: 디자인 수정, score 페이지 틀 작성, 대학 로고 이미지 업로드 기능 추가
    index 페이지의 대학교 목록과 점수를 table로 보여줌
    대학교에 점수를 매길 score 페이지를 만듦 (틀만 작성함)
    대학교의 로고 사진을 업로드할 수 있는 기능 추가 -> 관리자만 업로드할 수 있도록 수정 예정

--- application.py
+++ application.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 import requests
 from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///school.db'  # SQLite 사용 예시
 db = SQLAlchemy(app)
 
+photos = UploadSet('photos', IMAGES)
+app.config['UPLOADED_PHOTOS_DEST'] = 'K-unirank/media/school_image'  # 이미지가 저장될 경로 설정
+
+configure_uploads(app, photos)
+
 
 class School(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    schoolname = db.Column(db.String(100), unique=True)
+    school_name = db.Column(db.String(100), unique=True)
+    school_image = db.Column(db.String(200))
     score = db.Column(db.String(100))
 
 
@@ -28,9 +37,9 @@ data = response.json()
 @app.route("/")
 def index():
     for item in data['dataSearch']['content']:
-        new_school = School(schoolname=item['schoolName'], score=0)
+        new_school = School(school_name=item['schoolName'], score=0)
         existing_school = School.query.filter_by(
-            schoolname=item['schoolName']).first()
+            school_name=item['schoolName']).first()
         if existing_school is None:
             db.session.add(new_school)
             db.session.commit()
@@ -42,5 +51,33 @@ def index():
     return render_template('index.html', schools=schools)
 
 
+@app.route("/score")
+def score():
+    schools = School.query.order_by(School.score.desc()).all()
+    selected_school = School.query.filter_by(school_name=request.args.get('school')).first()
+    return render_template('score.html', schools=schools, selected_school=selected_school)
+
+
+@app.route('/upload')
+def upload():
+    schools = School.query.order_by(School.score.desc()).all()
+    return render_template('upload.html', schools=schools)
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    school = request.form['school']
+    selected_school = School.query.filter_by(school_name=school).first()
+    if 'image' in request.files:
+        image = request.files['image']
+        image.save('K-unirank/media/school_image' + image.filename)
+        selected_school.school_image = 'K-unirank/media/school_image' + image.filename  # 이미지 정보를 데이터베이스에 추가
+        db.session.commit()
+        return 'Image uploaded successfully'
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
