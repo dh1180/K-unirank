4c62f5bdfea55902f6e36bbab835ebab8d8b84d9
항붕이 <mjdh1936@gmail.com>
2024-04-13 11:10:45 +0000

    feat: 대학교 인증 기능 추가, 휴대폰으로 웹사이트 접속 시 글자크기 작아지게 수정

--- user/urls.py
+++ user/urls.py
@@ -5,12 +5,13 @@ app_name = "user"
 
 urlpatterns = [
     path('', views.user_profile, name='user_profile'),
-    path('change_username/', views.change_username, name='change_username'),
+    path('change_username', views.change_username, name='change_username'),
     path('user_posts/', views.user_posts, name='user_posts'),
     path('user_comment_posts/', views.user_comment_posts, name='user_comment_posts'),
     path('user_like_posts/', views.user_like_posts, name='user_like_posts'),
     path('user_profile/user_delete', views.user_delete, name='user_delete'),
-    
+
     path('other_user_posts/<int:author>', views.other_user_posts, name='other_user_posts'),
 
+    path('university_certification', views.university_certification, name='university_certification'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,11 @@
 from community.models import Post, Comment
+from vote.models import School
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.utils import timezone
+import requests, json
 
 
 def user_profile(request):
@@ -68,3 +70,45 @@ def other_user_posts(request, author):
     page_obj = paginator.get_page(page_number)
     return render(request, 'user/other_user_posts.html', {'page_obj': page_obj, 'author': User.objects.get(pk=author), 'today': today})
 
+
+api_key = "06b36831-6622-49bc-a6f4-c1bc351edb1e"
+headers = {"Content-Type": "application/json"}
+
+
+def university_certification(request):
+    schools = School.objects.all()
+    if request.method == 'POST':
+        email = request.POST["email"]
+        univName = request.POST["univName"]
+        code = request.POST.get('code')
+
+        if code is None:
+            url = "https://univcert.com/api/v1/certify"
+            data = {
+              "key" : api_key,
+              "email" : email,
+              "univName" : univName,
+              "univ_check" : True
+            }
+        else:
+            url = "https://univcert.com/api/v1/certifycode"
+            data = {
+              "key" : api_key,
+              "email" : email,
+              "univName" : univName,
+              "code" : code,
+            }
+        json_data = json.dumps(data)
+        response = requests.post(url, data=json_data, headers=headers)
+        response_data = response.json()
+        print(response_data)
+        print(url)
+        if(response_data["success"] and url == "https://univcert.com/api/v1/certify"):
+            return render(request, 'user/university_certification.html', {'schools': schools, 'success': True, 'email': email, 'univName': univName})
+        elif(response_data["success"] and url == "https://univcert.com/api/v1/certifycode"):
+            request.user.university = response_data["univName"]
+            request.user.save()
+        else:
+            return render(request, 'user/university_certification.html', {'schools': schools, 'error': response_data["message"]})
+        return render(request, 'user/user_profile.html')
+    return render(request, 'user/university_certification.html', {'schools': schools})
