e15d3f5d1277a9df793219ddd67481a37a924715
dh1180 <mjdh1936@gmail.com>
2025-07-25 03:09:33 +0900

    feat: PreviousRank 모델 관리자 등록 추가
    - PreviousRank 모델을 Django 관리자에 등록하여 관리 기능을 추가함

--- vote/admin.py
+++ vote/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import School
+from .models import School, PreviousRank
 
 # Register your models here.
 admin.site.register(School)
+admin.site.register(PreviousRank)
